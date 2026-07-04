2cf2ceaebce2b013f71ccb2796e2b737fb7ee8c2
roshangurung-369 <roshan.gurung2025@mic.edu.np>
2026-07-04 18:02:09 +0545

    added Q&A page

--- JS/profile.js
+++ JS/profile.js
@@ -1,12 +1,38 @@
 const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
 
-if (currentUser) {
+const profileArea = document.getElementById("profileArea");
+const profileMenu = document.getElementById("profileMenu");
+const dropdownMenu = document.getElementById("dropdownMenu");
+const registerNav = document.getElementById("registerNav");
+
+if(currentUser){
+
+    profileArea.style.display = "block";
+
     document.getElementById("profilePic").src = currentUser.profilePic;
     document.getElementById("profileName").textContent = currentUser.username;
 
-    const registerNav = document.getElementById("registerNav");
-
-    if (registerNav) {
+    if(registerNav){
         registerNav.style.display = "none";
     }
+
+    profileMenu.onclick = function(){
+
+        if(dropdownMenu.style.display === "block"){
+            dropdownMenu.style.display = "none";
+        }
+        else{
+            dropdownMenu.style.display = "block";
+        }
+
+    };
+
+    document.getElementById("logoutBtn").onclick = function(){
+
+        sessionStorage.removeItem("currentUser");
+
+        window.location.href = "register.html";
+
+    };
+
 }
